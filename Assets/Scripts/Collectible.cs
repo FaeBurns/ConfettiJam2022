8574cfc1c7fb457f775353e489ff269c57ec2693
bean <titless.skittle@gmail.com>
2022-06-19 23:10:27 +0100

    Implemented collectible and win system

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeanLib.References;
+using UnityEngine;
+
+/// <summary>
+/// Component responsible for collecting items.
+/// </summary>
+public class Collectible : ReferenceResolvedBehaviour
+{
+    [AutoReference] private CollectibleGoalManager goalManager;
+
+    [Min(1f)]
+    [SerializeField] private float scaleMagnitude;
+    [SerializeField] private float timescale = 1f;
+
+    private event Action<Sprite> OnCollect;
+
+    /// <inheritdoc/>
+    public override void Start()
+    {
+        base.Start();
+
+        OnCollect += goalManager.OnCollect;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(Vector3.one, new Vector3(scaleMagnitude, scaleMagnitude, scaleMagnitude), Mathf.Sin(Time.time * timescale));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OnCollect(GetComponentInChildren<SpriteRenderer>().sprite);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CollectibleGoalManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeanLib.References;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Component repsonsible for tracking the collected collectibles and the progress towards the goal.
+/// </summary>
+public class CollectibleGoalManager : ReferenceResolvedBehaviour
+{
+    private Vector2 startPosition;
+
+    [BindComponent] private Animator animator;
+
+    [SerializeField] private GameObject showcasePrefab;
+
+    [SerializeField] private RectTransform showcaseParent;
+    [SerializeField] private RectTransform finalParent;
+    [SerializeField] private RectTransform animatedShowcaseElement;
+    [SerializeField] private List<Image> showcaseImages;
+
+    [SerializeField] private Sprite finalSprite;
+
+    [SerializeField] private float lerpAlpha = 0;
+
+    [SerializeField] private int collectedGoal = 4;
+
+    /// <summary>
+    /// Shows the collected item in the UI and displays an animation.
+    /// </summary>
+    /// <param name="collectedSprite">The sprite to display.</param>
+    public void OnCollect(Sprite collectedSprite)
+    {
+        GameObject newElement = Instantiate(showcasePrefab, animatedShowcaseElement);
+
+        Image newImage = newElement.GetComponent<Image>();
+        newImage.sprite = collectedSprite;
+
+        if (showcaseImages.Count + 1 == collectedGoal)
+        {
+            newElement.transform.SetParent(finalParent, true);
+            animator.Play("Final");
+            ANIMFUNC_MoveAllBack();
+
+            showcaseImages.Add(newImage);
+            return;
+        }
+
+        showcaseImages.Add(newImage);
+
+        animator.Play("New");
+    }
+
+    /// <summary>
+    /// Animation Function - set parent.
+    /// </summary>
+    public void ANIMFUNC_MoveParent()
+    {
+        showcaseImages.Last().transform.SetParent(showcaseParent, true);
+    }
+
+    /// <summary>
+    /// Animation Function - move all back to animated element.
+    /// </summary>
+    public void ANIMFUNC_MoveAllBack()
+    {
+        foreach (Image image in showcaseImages)
+        {
+            image.transform.SetParent(animatedShowcaseElement, false);
+        }
+    }
+
+    /// <summary>
+    /// Animation Function - replace the set of images with one whole image.
+    /// </summary>
+    public void ANIMFUNC_ReplaceWithWhole()
+    {
+        foreach (Image image in showcaseImages)
+        {
+            Destroy(image.gameObject);
+        }
+
+        Instantiate(showcasePrefab, animatedShowcaseElement).GetComponent<Image>().sprite = finalSprite;
+    }
+
+    /// <summary>
+    /// Animation Function - end the game.
+    /// </summary>
+    public void ANIMFUNC_EndGame()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+    private void Awake()
+    {
+        ReferenceStore.ReplaceReference(this);
+
+        startPosition = animatedShowcaseElement.transform.position;
+    }
+
+    private void Update()
+    {
+        animatedShowcaseElement.transform.position = Vector2.Lerp(startPosition, showcaseParent.transform.position, lerpAlpha);
+    }
+}
