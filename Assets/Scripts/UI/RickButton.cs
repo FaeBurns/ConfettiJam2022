23cc9767cf22c1c8daffc7e231ac58cabdfebc0f
Fae Burns <titless.skittle@gmail.com>
2022-06-21 13:47:25 +0100

    Updated end screen

---
+++ Assets/Scripts/UI/RickButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class RickButton : MonoBehaviour
+{
+    const string url = @"https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+    public void Click()
+    {
+        Application.OpenURL(url);
+    }
+}
